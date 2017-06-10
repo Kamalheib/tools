67117ee9832536521e0231f6a512645ff4914b96
Kamal Heib <kamalheib1@gmail.com>
2017-06-11 00:10:31 +0300

    vpn_connect: Update the authgroup token
    Update the authgroup token to the new token.
    
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- scripts/vpn_connect.sh
+++ scripts/vpn_connect.sh
@@ -2,4 +2,4 @@
 
 USER=$1
 
-sudo openconnect --servercert=sha256:cd8bc44a2b2d6d05eecceb80f0ffd43e5d569245a451a6c19e5e47a9d486b687 --protocol=nc ssl-il.mellanox.com --pid-file="/var/run/work-vpn.pid" --user=$USER  --authgroup="Mellanox_VIP_App/Token"
+sudo openconnect --servercert=sha256:cd8bc44a2b2d6d05eecceb80f0ffd43e5d569245a451a6c19e5e47a9d486b687 --protocol=nc ssl-il.mellanox.com --pid-file="/var/run/work-vpn.pid" --user=$USER  --authgroup="Mellanox_VIP_Access_App/Token_Auth"
