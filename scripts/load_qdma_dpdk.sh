beff4bf61d014ddfe0b79c914fcfd3b4e5ab02cf
Kamal Heib <kamalheib1@gmail.com>
2018-07-02 14:28:39 +0300

    add qdma_mm_cmds to run with load_qdma_dpdk
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- scripts/load_qdma_dpdk.sh
+++ scripts/load_qdma_dpdk.sh
@@ -4,4 +4,4 @@ mount -t hugetlbfs nodev /mnt/huge
 modprobe -v uio
 insmod /work/dpdk/build/kmod/igb_uio.ko
 python /work/dpdk/tools/dpdk-devbind.py -b igb_uio 02:00.0
-/work/dpdk/examples/qdma_testapp/build/app/qdma_testapp -c 0xf -n 4 --
+/work/dpdk/examples/qdma_testapp/build/app/qdma_testapp -c 0xf -n 4 -- --f /work/tools/scripts/qdma_mm_cmds.txt
