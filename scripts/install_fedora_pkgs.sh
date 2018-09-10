0fd3fddcb833a7dbab6401ba144385b4ec233d49
Kamal Heib <kamalheib1@gmail.com>
2018-09-10 11:13:22 +0300

    scripts: Add GitPython package to install
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- scripts/install_fedora_pkgs.sh
+++ scripts/install_fedora_pkgs.sh
@@ -2,4 +2,4 @@
 
 sudo dnf update -y 
 sudo dnf groupinstall "C Development Tools and Libraries" -y 
-sudo dnf install -y thunderbird gvim meld hexchat powerline vim-powerline openssl-devel ncurses-devel bc elfutils-libelf-devel mutt gnome-tweak-tool krb5-workstation
+sudo dnf install -y thunderbird gvim meld hexchat powerline vim-powerline openssl-devel ncurses-devel bc elfutils-libelf-devel mutt gnome-tweak-tool krb5-workstation GitPython
