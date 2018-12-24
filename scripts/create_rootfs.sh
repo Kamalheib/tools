ce3d403fb58c2fff200dcfb1179af116d5de2946
Kamal Heib <kamalheib1@gmail.com>
2018-12-24 11:00:22 +0200

    scripts: Introduce build rootfs script
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

---
+++ scripts/create_rootfs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+dnf install -y bison openssl-devel ncurses-devel bc elfutils-libelf-devel flex
+
+mkdir -p /tmp/work/rootfs
+cd /tmp/work/rootfs
+dnf groupinstall "Fedora Server Edition" --releasever=29 --installroot=`pwd` --repo=fedora --repo=updates -y
+dnf install -y --releasever=29 --installroot=`pwd` --repo=fedora --repo=updates rdma-core
+sed -i 's/root:\*:/root::/g' etc/shadow
+
+cd /tmp/work
+git clone https://github.com/torvalds/linux.git
+cd linux/
+git checkout -b v4.19 v4.19
+cp /boot/config-`uname -r` .
+make olddefconfig
+make -j 32
+make -j 32 modules_install INSTALL_MOD_PATH=/tmp/work/rootfs
+cd /tmp/work/rootfs
+find . | cpio -o --format=newc > ../rootfs.img
