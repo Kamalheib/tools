01308ac8773f91a1d2de7089fb732d1b6ff6f917
Kamal Heib <kamalheib1@gmail.com>
2018-12-24 16:48:31 +0200

    scripts: qemu_start: Support create pvrdma device
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- scripts/qemu_start.sh
+++ scripts/qemu_start.sh
@@ -8,10 +8,14 @@ cd ~/work/qemu/
 	-s \
 	-cpu host \
 	-smp cpus=4 \
-	-m 2G \
+	-m 4G \
 	-nographic \
 	-enable-kvm  \
 	-kernel $KERNEL \
 	-initrd $ROOTFS \
 	-append "root=/dev/ram rdinit=/sbin/init  console=ttyS0 nokaslr" \
-	-object memory-backend-ram,id=mb1,size=1G,share \
+	-object memory-backend-ram,id=mb1,size=2G,share \
+	-chardev socket,path=/var/run/rdmacm-mux-mlx4_0-2,id=mads \
+	-netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
+	-device vmxnet3,addr=10.0,multifunction='on',netdev=net0 \
+	-device pvrdma,addr=10.1,ibdev=mlx4_0,netdev=tap0,mad-chardev=mads \
