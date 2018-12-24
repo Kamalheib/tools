d24995024deb7ecce0794dd12eb7b2feabc3f308
Kamal Heib <kamalheib1@gmail.com>
2018-12-24 13:22:32 +0200

    scripts: Introduce qemu_start.sh script
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

---
+++ scripts/qemu_start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+KERNEL=/tmp/work/linux/arch/x86/boot/bzImage
+ROOTFS=/tmp/work/rootfs.img
+
+cd ~/work/qemu/
+./build/x86_64-softmmu/qemu-system-x86_64 \
+	-s \
+	-cpu host \
+	-smp cpus=4 \
+	-m 1G \
+	-nographic \
+	-enable-kvm  \
+	-kernel $KERNEL \
+	-initrd $ROOTFS \
+	-append "root=/dev/ram rdinit=/sbin/init  console=ttyS0 nokaslr" \
+	-object memory-backend-ram,id=mb1,size=1G,share \
+	-numa node,memdev=mb1 \
