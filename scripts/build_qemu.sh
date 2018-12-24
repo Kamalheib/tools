9cdd6c5c905787ce9e7cf4fa5fcdcc76cecfcb7d
Kamal Heib <kamalheib1@gmail.com>
2018-12-24 10:19:36 +0200

    scripts: introduce qemu build script
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

---
+++ scripts/build_qemu.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+yum install -y pixman-devel jemalloc-devel libcap-devel libattr-devel
+mkdir -p ~/work/
+cd ~/work
+git clone https://github.com/qemu/qemu.git
+cd qemu
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var \
+         --libexecdir=/usr/lib/qemu --python=/usr/bin/python2 \
+         --smbd=/usr/bin/smbd --with-sdlabi=2.0 --enable-modules \
+         --enable-jemalloc --enable-debug --target-list=x86_64-softmmu --enable-virtfs --enable-rdma
+make -j 16
