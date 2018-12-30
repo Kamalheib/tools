02f1ecfd78cd41d784aed8c5716bcb7b624f3528
Kamal Heib <kamalheib1@gmail.com>
2018-12-30 23:51:19 +0200

    scripts: Update path and config file
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- scripts/create_rootfs.sh
+++ scripts/create_rootfs.sh
@@ -2,19 +2,20 @@
 
 dnf install -y bison openssl-devel ncurses-devel bc elfutils-libelf-devel flex
 
-mkdir -p /tmp/work/rootfs
-cd /tmp/work/rootfs
+mkdir -p /home/rootfs
+cd /home/rootfs
 dnf groupinstall "Minimal Install" --releasever=29 --installroot=`pwd` --repo=fedora --repo=updates -y
 dnf install -y --releasever=29 --installroot=`pwd` --repo=fedora --repo=updates rdma-core pciutils infiniband-diags libibverbs-utils
 sed -i 's/root:\*:/root::/g' etc/shadow
 
-cd /tmp/work
+cd /home
 git clone https://github.com/torvalds/linux.git
 cd linux/
 git checkout -b v4.19 v4.19
-cp /boot/config-`uname -r` .
+wget https://raw.githubusercontent.com/Kamalheib/configs/master/linux/config
+mv config .config
 make olddefconfig
 make -j 32
-make -j 32 modules_install INSTALL_MOD_PATH=/tmp/work/rootfs
-cd /tmp/work/rootfs
+make -j 32 modules_install INSTALL_MOD_PATH=/home/rootfs
+cd /home/rootfs
 find . | cpio -o --format=newc > ../rootfs.img
