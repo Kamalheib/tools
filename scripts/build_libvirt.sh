e15d8d63b28269b7ca6bc4a72d86082af3dc9b37
Kamal Heib <kamalheib1@gmail.com>
2019-01-08 11:51:02 +0200

    scripts: build_libvirt: Install required packages
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- scripts/build_libvirt.sh
+++ scripts/build_libvirt.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+dnf install -y libtool rpcgen gnutls-devel libnl3-devel glibc2 glibc-devel mingw32-portablexdr mingw64-portablexdr
 cd /home
 git clone http://github.com/kamalheib/libvirt
-for p in `cat libvirt.spec.in | grep "BuildRequires" | cut -d " " -f 2`; do dnf install -y $p; done
 cd libvirt
+dnf install -y `cat libvirt.spec.in | grep "BuildRequires" | cut -d " " -f 2 | grep -v python`
 ./autogen.sh
