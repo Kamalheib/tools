60d8bca1e42afa5414ee687ff8cd47ac3c781284
Kamal Heib <kamalheib1@gmail.com>
2019-01-18 15:09:07 +0200

    scripts: build_libvirt: use yum to install packages
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- scripts/build_libvirt.sh
+++ scripts/build_libvirt.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-dnf install -y libtool rpcgen gnutls-devel libnl3-devel glibc2 glibc2-devel mingw32-portablexdr mingw64-portablexdr
+yum install -y libtool rpcgen gnutls-devel libnl3-devel glibc2 glibc2-devel mingw32-portablexdr mingw64-portablexdr
 cd /home
 git clone http://github.com/kamalheib/libvirt
 cd libvirt
-dnf install -y `cat libvirt.spec.in | grep "BuildRequires" | cut -d " " -f 2 | grep -v python`
+yum install -y `cat libvirt.spec.in | grep "BuildRequires" | cut -d " " -f 2 | grep -v python`
 ./autogen.sh --prefix=$HOME/usr
 make -j 16
 make -j install
