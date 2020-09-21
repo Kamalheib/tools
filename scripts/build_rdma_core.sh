acffb98044d926dc1e78a6de548fddccc6bd407f
Kamal Heib <kamalheib1@gmail.com>
2020-09-21 11:43:55 +0300

    scripts/build_rdma_core: clone rdma-core repo
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- scripts/build_rdma_core.sh
+++ scripts/build_rdma_core.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 dnf install -y cmake gcc libnl3-devel libudev-devel pkgconfig valgrind-devel ninja-build python3-devel python3-Cython python3-docutils pandoc
+cd /home
+git clone https://github.com/kamalheib/rdma-core
 cd /home/rdma-core
 bash build.sh
