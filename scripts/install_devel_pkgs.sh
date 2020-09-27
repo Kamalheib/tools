adad07c59d07381fd2382829bf649e5fb20b3635
Kamal Heib <kamalheib1@gmail.com>
2020-09-27 14:44:12 +0300

    scripts: install_devel_pkgs: add bison and flex
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- scripts/install_devel_pkgs.sh
+++ scripts/install_devel_pkgs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #RDMA core
-yum install -y cmake gcc libnl3-devel libudev-devel pkgconfig valgrind-devel ninja-build python3-devel python3-Cython python3-docutils pandoc
+yum install -y cmake gcc libnl3-devel libudev-devel pkgconfig valgrind-devel ninja-build python3-devel python3-Cython python3-docutils pandoc bison flex
