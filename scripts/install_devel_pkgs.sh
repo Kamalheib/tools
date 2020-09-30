635405be61d2bc7b9a1a8f43a86c526b8e777c22
Kamal Heib <kamalheib1@gmail.com>
2020-09-30 13:08:54 +0300

    install_devel_pkgs: Add section for iproute2
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- scripts/install_devel_pkgs.sh
+++ scripts/install_devel_pkgs.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 #RDMA core
-yum install -y cmake gcc libnl3-devel libudev-devel pkgconfig valgrind-devel ninja-build python3-devel python3-Cython python3-docutils pandoc bison flex libmnl-devel libcap-devel
+yum install -y cmake gcc libnl3-devel libudev-devel pkgconfig valgrind-devel ninja-build python3-devel python3-Cython python3-docutils pandoc
+
+#iproute2
+yum install -y bison flex libmnl-devel libcap-devel
